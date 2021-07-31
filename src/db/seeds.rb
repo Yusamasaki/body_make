075898e7bab666d013f91ac0b35de3d713f9958c
EC2 Default User <ec2-user@ip-172-31-45-227.ap-northeast-1.compute.internal>
2021-07-31 07:02:10 +0000

    devise.view.ja追加

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+    email: "otsukkomasaki.1208@icloud.com",
+    password: "password",
+    username: "test"
+    )
