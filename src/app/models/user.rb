a241373d0c10f0016f49391c415295a7458c856d
EC2 Default User <ec2-user@ip-172-31-45-227.ap-northeast-1.compute.internal>
2021-08-02 07:21:00 +0000

    devise一から修正

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  def self.new_with_session(params, session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"]) do |user|
+        user.attributes = params
+      end
+    else
+      super
+    end
+  end
 end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
 
+devise_for :users
+
 root to: 'staticpages#top'
 
-devise_for :users, :controllers => {
-  :registrations => 'users/registrations',
-  :sessions => 'users/sessions',
-  :passwords => 'users/passwords'
-} 
-
-devise_scope :user do
-  get "user/:id", :to => "users/registrations#detail"
-  get "signup", :to => "users/registrations#new"
-  get "login", :to => "users/sessions#new"
-  get "logout", :to => "users/sessions#destroy"
-  get "password", :to => "users/passwords#new"
+resources :users do
 end
+
 
 end
