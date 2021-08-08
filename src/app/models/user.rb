f64a1a9ba7a83bfd86da483ff17437fa63a592d5
EC2 Default User <ec2-user@ip-172-31-45-227.ap-northeast-1.compute.internal>
2021-08-08 12:56:29 +0000

    最新

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[facebook google_oauth2]
          
   def self.new_with_session(params, session)
     if session["devise.user_attributes"]
