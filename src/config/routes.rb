80dfd982122b61f9c31588977b129261c89bbae6
yusamasaki <masa.aw1208@gmail.com>
2021-08-12 09:13:55 +0900

    homecontroller作成

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
+  
+  root to: 'staticpages#top'
+  get 'homes/show'
 
   devise_for :users,
-  controllers: {
-    sessions: 'users/sessions',
-    registrations: "users/registrations",
-    omniauth_callbacks: 'users/omniauth_callbacks'
-  }
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: "users/registrations",
+      omniauth_callbacks: 'users/omniauth_callbacks'
+    }
 
-root to: 'staticpages#top'
-
-resources :users do
-end
+  resources :users do
+  end
 
 
 end
