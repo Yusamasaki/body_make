5f41119140ea8996ab1b893bdb21537a10bb1469
yusamasaki <masa.aw1208@gmail.com>
2021-08-14 02:30:57 +0900

    modelバリデーション設定

--- src/app/models/bmr.rb
+++ src/app/models/bmr.rb
@@ -1,2 +1,7 @@
 class Bmr < ApplicationRecord
+  belongs_to :user
+
+  validates :gender, presence: true
+  validates :age, presence: true, length: { maximum: 3 }
+  validates :height, presence: true, length: { maximum: 3 } 
 end
