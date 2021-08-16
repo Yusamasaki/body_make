a41b0327383252eafe7634f8fabf7de10d33a17c
yusamasaki <masa.aw1208@gmail.com>
2021-08-16 21:15:16 +0900

    new-form create

--- src/app/controllers/bodyweights_controller.rb
+++ src/app/controllers/bodyweights_controller.rb
@@ -3,9 +3,18 @@ class BodyweightsController < ApplicationController
   end
 
   def new
+    @body_weight = current_user.bodyweights.new
   end
 
   def create
+    @body_weight = current_user.bodyweights.new(body_weight_params)
+    if @body_weight.save
+      flash[:success] = "新規作成に成功しました。"
+      redirect_to ""
+    else
+      flash[:danger] = "新規作成に失敗しました。"
+      redirect_to ""
+    end
   end
 
   def show
@@ -16,4 +25,9 @@ class BodyweightsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def body_weight_params
+      params.require(:bodyweight).permit(:body_weight, :bodyfat_percentage)
+    end
 end
