c1a60ec14868068e5589a37d3156e331e2d05ba2
yusamasaki <masa.aw1208@gmail.com>
2021-08-25 13:11:01 +0000

    グラフ機能追加

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -3,6 +3,13 @@ class UsersController < ApplicationController
   def show
     @body_weight = current_user.bodyweights.find_by(id: params[:bodyweight_id])
     @body_weights = current_user.bodyweights.all
+    
+    # グラフ機能仮置き
+    @first_day = params[:start_date].nil? ?
+    Date.current.beginning_of_month : params[:start_date].to_date
+    @last_day = @first_day.end_of_month
+    one_month = [*@first_day..@last_day]
+    gon.day = one_month
   end
   
 end
--- src/app/models/bodyweight.rb
+++ src/app/models/bodyweight.rb
@@ -1,4 +1,5 @@
 class Bodyweight < ApplicationRecord
   belongs_to :user
 
+  validates :body_weight, presence: true
 end
