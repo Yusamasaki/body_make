8fdbedd25dba31cf36c8c9c57b0676f5558628c4
kibwwen <kibwwen@gmail.com>
2021-08-30 23:55:28 +0900

    delete webpacker adn fix set_user bug

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,15 +1,10 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-  def set_user
-    @user = User.find(current_user.id)
-=======
   
   def set_user
-    @user = User.current_user.id
+    @user = User.find(current_user.id)
   end
   
   # BodyWeightクラスの1ヶ月分start_time(日にち)を作成
-  
   def bodyweight_set_one_month
     @first_day = params[:start_date].nil? ?
     Date.current.beginning_of_month : params[:start_date].to_date
@@ -25,6 +20,5 @@ class ApplicationController < ActionController::Base
       end
       @bodyweights = current_user.bodyweights.where(start_time: @first_day..@last_day).order(:start_time)
     end
->>>>>>> f28f255c54dac697ec39ac6baf24184af92281a5
   end
 end
