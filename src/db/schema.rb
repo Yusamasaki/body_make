ef42a5c1bfe685ea86b238a12b07873cada89d74
kibwwen <kibwwen@gmail.com>
2021-09-19 21:20:45 +0900

    Fix conflict

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_09_18_155740) do
-=======
 ActiveRecord::Schema.define(version: 2021_09_12_133104) do
->>>>>>> develop
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
