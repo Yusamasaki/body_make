571c6a6583acd0615cdcd4f7c264ebe14e83078c
EC2 Default User <ec2-user@ip-172-31-33-142.ap-northeast-1.compute.internal>
2021-09-28 21:49:07 +0000

    today_traning機能実装

--- src/app/controllers/traningevents_controller.rb
+++ src/app/controllers/traningevents_controller.rb
@@ -7,6 +7,7 @@ class TraningeventsController < ApplicationController
     @traningevents_all = @user.traningevents.all
     @traningtypes = Traningtype.all
     @bodyparts = Bodypart.where(id: 2..6)
+    @today_traning = @user.today_tranings.find(params[:today_traning_id])
   end
   
   def new
@@ -14,25 +15,29 @@ class TraningeventsController < ApplicationController
     @traningtype = Traningtype.find(params[:traningtype_id])
     @sub_bodyparts = @bodypart.sub_bodyparts.all.map {|sub_bodypart| [sub_bodypart.sub_body_part, sub_bodypart.sub_body_part]}
     @traningevent = @user.traningevents.new
+    @today_traning = @user.today_tranings.find(params[:today_traning_id])
   end
   
   def create
+    @today_traning = @user.today_tranings.find(params[:today_traning_id])
     @traningevent = @user.traningevents.new(traningevent_params)
     if @traningevent.save
-      redirect_to user_traningevents_path(current_user, start_date: params[:start_date], start_time: params[:start_time])
+      redirect_to user_traningevents_path(current_user, today_traning_id: @today_traning, start_date: params[:start_date], start_time: params[:start_time])
     else
       flash[:danger] = "登録に失敗しました。"
-      redirect_to new_user_traningevent_path(current_user, start_date: params[:start_date], start_time: params[:start_time])
+      redirect_to new_user_traningevent_path(current_user, today_traning_id: @today_traning, start_date: params[:start_date], start_time: params[:start_time])
     end
   end
 
   def edit
+    @today_traning = @user.today_tranings.find(params[:today_traning_id])
     @traningevent = @user.traningevents.find(params[:id])
     @body_part = Bodypart.find_by(body_part: @traningevent.body_part)
     @sub_bodyparts = @body_part.sub_bodyparts.all.map {|sub_bodypart| [sub_bodypart.sub_body_part, sub_bodypart.sub_body_part]}
   end
 
   def update
+    @today_traning = @user.today_tranings.find(params[:today_traning_id])
     @traningevent = @user.traningevents.find(params[:id])
     if @traningevent.update_attributes!(traningevent_params)
       flash[:success] = "更新に成功しました"
@@ -44,10 +49,12 @@ class TraningeventsController < ApplicationController
   end
 
   def show
+    @today_traning = @user.today_tranings.find(params[:today_traning_id])
     @traningevent = @user.traningevents.find(params[:id])
   end
 
   def destroy
+    @today_traning = @user.today_tranings.find(params[:today_traning_id])
     @traningevent = @user.traningevents.find(params[:id])
     @traningevent.destroy
     flash[:success] = "削除しました。"
--- src/db/schema.rb
+++ src/db/schema.rb
@@ -154,8 +154,8 @@ ActiveRecord::Schema.define(version: 2021_09_28_075310) do
     t.float "goal_body_weight"
     t.float "now_bodyfat_percentage"
     t.float "goal_bodyfat_percentage"
-    t.datetime "beginning_date", default: "2021-09-28 12:34:32"
-    t.datetime "target_date", default: "2021-09-29 12:34:32"
+    t.datetime "beginning_date", default: "2021-09-28 21:13:16"
+    t.datetime "target_date", default: "2021-09-29 21:13:16"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -169,8 +169,8 @@ ActiveRecord::Schema.define(version: 2021_09_28_075310) do
   end
 
   create_table "today_exercises", force: :cascade do |t|
-    t.date "start_time", default: "2021-09-28", null: false
-    t.datetime "exercise_time", default: "2021-09-27 15:00:00", null: false
+    t.date "start_time", default: "2021-09-29", null: false
+    t.datetime "exercise_time", default: "2021-09-28 15:00:00", null: false
     t.string "note"
     t.bigint "exercise_category_id"
     t.bigint "user_id"
