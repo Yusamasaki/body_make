8b29855af185a18ef7cb196bd1974b15dd721efe
Tadakiyo-Hirano <d85nus03@gmail.com>
2021-11-30 17:23:25 +0900

    TodayExerciseバリデーション修正

--- src/app/models/today_exercise.rb
+++ src/app/models/today_exercise.rb
@@ -2,8 +2,8 @@ class TodayExercise < ApplicationRecord
   belongs_to :user
 
   validates :start_time, presence: true
-  validates :exercise_time_hour, presence: true
-  validates :exercise_time_min, presence: true
+  validates :exercise_time_hour, presence: true, numericaliity: { greater_than_or_equal_to: 0, less_than_or_equal_to: 23 }
+  validates :exercise_time_min, presence: true, numericaliity: { greater_than_or_equal_to: 0, less_than_or_equal_to: 55 }
   validates :note, length: { maximum: 30 }
   # validates :exercise_category_id, presence: true
   # validates :exercise_content_id, presence: true
