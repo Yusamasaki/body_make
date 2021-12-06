e794d88b9be8e677031ef99a567c6348c2cbcb7b
Tadakiyo-Hirano <d85nus03@gmail.com>
2021-12-06 17:27:53 +0900

    gem database_cleaner追加

--- src/spec/models/exercise_content_spec.rb
+++ src/spec/models/exercise_content_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ExerciseContent, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:exercise_content) { FactoryBot.build(:exercise_content) }
+  aaa = FactoryBot.create(:exercise_category)
+  pp aaa
 end
