16ddd620b8cd3f58863701e7f3eca1c4e6933f6e
masakiyusa <masa.aw1208@gmail.com>
2021-12-07 03:27:21 +0000

    最新

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -151,8 +151,8 @@ ActiveRecord::Schema.define(version: 2021_10_26_000142) do
     t.float "goal_body_weight"
     t.float "now_bodyfat_percentage"
     t.float "goal_bodyfat_percentage"
-    t.datetime "beginning_date", default: "2021-12-01 22:07:23"
-    t.datetime "target_date", default: "2021-12-02 22:07:23"
+    t.datetime "beginning_date", default: "2021-12-07 03:21:18"
+    t.datetime "target_date", default: "2021-12-08 03:21:18"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -166,8 +166,8 @@ ActiveRecord::Schema.define(version: 2021_10_26_000142) do
   end
 
   create_table "today_exercises", force: :cascade do |t|
-    t.date "start_time", default: "2021-12-02", null: false
-    t.datetime "exercise_time", default: "2021-12-01 15:00:00", null: false
+    t.date "start_time", default: "2021-12-07", null: false
+    t.datetime "exercise_time", default: "2021-12-06 15:00:00", null: false
     t.float "body_weight"
     t.string "note"
     t.bigint "exercise_category_id"
