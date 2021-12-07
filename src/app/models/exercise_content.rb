5ede19b3b2aca7e28ae1a3be1b8622ca83197e3e
Tadakiyo-Hirano <d85nus03@gmaul.com>
2021-12-07 21:33:02 +0900

    exercise_content rspec作成

--- src/app/models/exercise_content.rb
+++ src/app/models/exercise_content.rb
@@ -2,5 +2,5 @@ class ExerciseContent < ApplicationRecord
   belongs_to :exercise_category
 
   validates :content, presence: true, length: { maximum: 20 }
-  validates :mets, presence: true, length: { maximum: 10 }
+  validates :mets, presence: true, numericality: { less_than: 999 }
 end
--- src/spec/models/exercise_content_spec.rb
+++ src/spec/models/exercise_content_spec.rb
@@ -1,7 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe ExerciseContent, type: :model do
-  let(:exercise_content) { FactoryBot.build(:exercise_content) }
-  aaa = FactoryBot.create(:exercise_category)
-  pp aaa
+  let(:exercise_content) { FactoryBot.create(:exercise_content) }
+
+  describe '運動コンテンツを登録する' do
+
+    context 'コンテンツ名の存在が' do
+      it 'あれば有効な状態であること' do
+        expect(exercise_content).to be_valid
+      end
+
+      it 'なければ無効な状態であること' do
+        exercise_content.content = nil
+        exercise_content.valid?
+        expect(exercise_content.errors[:content]).to include('を入力してください')
+        pp exercise_content
+      end
+    end
+
+    context 'metsの存在が' do
+      it 'あれば有効な状態であること' do
+        expect(exercise_content).to be_valid
+      end
+
+      it 'なければ無効な状態であること' do
+        exercise_content.mets = nil
+        exercise_content.valid?
+        expect(exercise_content.errors[:mets]).to include('を入力してください')
+      end
+    end
+
+  end
 end
