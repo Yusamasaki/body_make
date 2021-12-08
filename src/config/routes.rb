cd6afc34bacb9f76817b9c467d175d3b41284eb9
kibwwen <kibwwen@gmail.com>
2021-12-09 02:26:57 +0900

    12/8 作業分

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   devise_scope :user do
-    root :to => "devise/sessions#new"
+    root to: "devise/sessions#new"
   end
 
   devise_for :users,
--- src/db/schema.rb
+++ src/db/schema.rb
@@ -151,8 +151,8 @@ ActiveRecord::Schema.define(version: 2021_10_26_000142) do
     t.float "goal_body_weight"
     t.float "now_bodyfat_percentage"
     t.float "goal_bodyfat_percentage"
-    t.datetime "beginning_date", default: "2021-12-08 12:10:53"
-    t.datetime "target_date", default: "2021-12-09 12:10:53"
+    t.datetime "beginning_date", default: "2021-12-08 15:53:12"
+    t.datetime "target_date", default: "2021-12-09 15:53:12"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -166,7 +166,7 @@ ActiveRecord::Schema.define(version: 2021_10_26_000142) do
   end
 
   create_table "today_exercises", force: :cascade do |t|
-    t.date "start_time", default: "2021-12-08", null: false
+    t.date "start_time", default: "2021-12-09", null: false
     t.integer "exercise_time_hour", default: 0, null: false
     t.integer "exercise_time_min", default: 0, null: false
     t.float "body_weight"
