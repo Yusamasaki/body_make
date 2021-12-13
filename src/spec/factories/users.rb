f1a568f83d423524015e306494f3e6b4c7d520d2
masakiyusa <masa.aw1208@gmail.com>
2021-12-13 01:28:51 +0000

    userモデルrspec完

--- src/spec/factories/users.rb
+++ src/spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    username {"user"}
+    sequence(:email) { |n| "user#{n}@email.com"}
+    password {"password"}
+    password_confirmation {'password'}
   end
 end
