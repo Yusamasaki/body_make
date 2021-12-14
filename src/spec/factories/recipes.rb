4afdd579c93156f118759cf1a1b88afb14ea77ef
masakiyusa <masa.aw1208@gmail.com>
2021-12-14 07:24:46 +0000

    recipeモデルrspec 完

--- src/spec/factories/recipes.rb
+++ src/spec/factories/recipes.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :recipe do
-    
+    recipe_name { "豚肉と野菜炒め" }
+    calorie { 500 }
+    protein { 50 }
+    fat { 50 }
+    carbo { 50 }
+    sugar { 50 }
+    dietary_fiber { 50 }
+    salt { 50 }
+    association :user
   end
 end
--- src/spec/models/recipe_spec.rb
+++ src/spec/models/recipe_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:recipe) { FactoryBot.create(:recipe) }
+  
+  describe "アソシエーションテスト" do
+    context "Userモデルとの関係" do
+      it "N:1となっていること" do
+        recipe = Recipe.reflect_on_association(:user)
+        expect(recipe.macro).to eq(:belongs_to)
+      end
+    end
+  end
+  
+  describe "レシピの登録" do
+    
+    it '重複したレシピ名があれば無効な状態であること' do
+      recipe1 = FactoryBot.create(:recipe)
+      recipe2 = FactoryBot.build(:recipe, recipe_name: recipe1.recipe_name)
+
+      recipe2.valid?
+      expect(recipe2.errors[:recipe_name]).to include('はすでに存在します')
+    end
+    
+    context "レシピ名の存在が" do
+      it "あれば有効な状態であること" do
+        expect(recipe).to be_valid
+      end
+      
+      it 'なければ無効な状態であること' do
+        recipe.recipe_name = nil
+        recipe.valid?
+        expect(recipe.errors[:recipe_name]).to include('を入力してください')
+      end
+    end
+    
+    context 'レシピ名の文字数が' do
+      it '100文字以内であれば有効な状態であること' do
+        recipe.recipe_name = 'a' * 100
+        expect(recipe).to be_valid
+      end
+
+      it '100文字以上であれば無効な状態であること' do
+        recipe.recipe_name = 'a' * 101
+        recipe.valid?
+        expect(recipe.errors[:recipe_name]).to include('は100文字以内で入力してください')
+      end
+    end
+  end
 end
