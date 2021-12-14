a7bcdfb34ffb329a3a76e89966358addbe3814ce
Tadakiyo-Hirano <d85nus03@gmail.com>
2021-12-14 17:26:50 +0900

    seedファイルユーザー数追加

--- src/app/controllers/admins_controller.rb
+++ src/app/controllers/admins_controller.rb
@@ -3,7 +3,7 @@ class AdminsController < ApplicationController
   before_action :authenticate_admin!, only: :show
 
   def show
-    @users = User.all.page(params[:page]).per(1).order(:id)
-    @categories = ExerciseCategory.all.page(params[:page]).per(1).order(:id)
+    @users = User.all.page(params[:user_page]).per(5).order(:id)
+    @categories = ExerciseCategory.all.page(params[:category_page]).per(3).order(:id)
   end
 end
