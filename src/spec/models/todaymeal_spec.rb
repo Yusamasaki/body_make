bb9555aaeeff226029701f64f520650d85580255
masakiyusa <masa.aw1208@gmail.com>
2021-12-15 06:08:21 +0000

    todaymeal, todaymeal_recipeモデルrspec完

--- src/spec/models/todaymeal_spec.rb
+++ src/spec/models/todaymeal_spec.rb
@@ -38,5 +38,40 @@ RSpec.describe Todaymeal, type: :model do
         expect(todaymeal.errors[:start_time]).to include('を入力してください')
       end
     end
+    
+    context "分量" do
+      it "あれば有効な状態であること" do
+        expect(todaymeal).to be_valid
+      end
+      
+      it 'なければ無効な状態であること' do
+        todaymeal.amount = nil
+        todaymeal.valid?
+        expect(todaymeal.errors[:amount]).to include('を入力してください')
+      end
+      
+      it '1以上であれば有効であること' do
+        todaymeal.amount >= 1
+        expect(todaymeal).to be_valid
+      end
+      
+      it '100以下であれば有効であること' do
+        todaymeal.amount <= 100
+        expect(todaymeal).to be_valid
+      end
+    end
+    
+    context "備考の文字数が" do
+      it '100文字以内であれば有効な状態であること' do
+        todaymeal.note = 'a' * 100
+        expect(todaymeal).to be_valid
+      end
+
+      it '100文字以上であれば無効な状態であること' do
+        todaymeal.note = 'a' * 101
+        todaymeal.valid?
+        expect(todaymeal.errors[:note]).to include('は100文字以内で入力してください')
+      end
+    end
   end
 end
