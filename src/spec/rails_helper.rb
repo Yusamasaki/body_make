6dbdf4cb8da1eb0a3cfadd9141f293c1009dc97c
masakiyusa <masa.aw1208@gmail.com>
2021-12-19 14:36:21 +0000

    最新

--- src/spec/rails_helper.rb
+++ src/spec/rails_helper.rb
@@ -5,6 +5,15 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+require 'capybara/rspec'
+Capybara.configure do |config|
+config.match = :prefer_exact
+end
+require 'selenium-webdriver'
+Selenium::WebDriver::Chrome.driver_path = "ここに自身の環境のpathを追加してください。"
+
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -30,6 +39,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   
   config.include Devise::Test::IntegrationHelpers, type: :request
