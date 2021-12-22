5e847b87d93f5d1bfa52e0260fa5461f6aec877e
Tadakiyo-Hirano <d85nus03@gmail.com>
2021-12-22 17:26:46 +0900

    exercise_category request spec create edit追加

--- src/spec/factories/exercise_categories.rb
+++ src/spec/factories/exercise_categories.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :exercise_category do
     name { "カテゴリー1" }
+
+    trait :invalid do
+      name { nil }
+    end
   end
 end
--- src/spec/requests/exercise_categories_spec.rb
+++ src/spec/requests/exercise_categories_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "ExerciseCategories", type: :request do
   let(:admin) { FactoryBot.create(:admin) }
   let(:exercise_category) { FactoryBot.create(:exercise_category) }
-  let(:exercise_content) { FactoryBot.create(:exercise_content) }
+  # let(:exercise_content) { FactoryBot.create(:exercise_content) }
 
   describe "GET index" do
     it "管理者でログイン時204httpレスポンスを返すこと" do
@@ -42,17 +42,66 @@ RSpec.describe "ExerciseCategories", type: :request do
   end
 
   describe "POST create" do
-    it "カテゴリーが作成できること" do
-      expect do
-        category_params = FactoryBot.attributes_for(:exercise_category)
-        sign_in admin
+    category_params = FactoryBot.attributes_for(:exercise_category)
 
-        expect {
-          post exercise_categories_path, params: { exercise_category: category_params }
-        }.to change(exercise_category, :count).by(1)
-        pp exercise_category
+    context "パラメータが妥当な場合" do
+      it "リクエストが成功すること" do
+        sign_in admin
+        post exercise_categories_path, params: { exercise_category: category_params }
+        expect(response.status).to eq 302
+      end
+
+      it "カテゴリーが登録できること" do
+        expect do
+          sign_in admin
+          expect {
+            post exercise_categories_path, params: { exercise_category: category_params }
+          }.to change(exercise_category, :count).by(1)
+        end
+      end
+
+      it "リダイレクトすること" do
+        sign_in admin
+        post exercise_categories_path, params: { exercise_category: category_params }
+        expect(response).to redirect_to admin_path(admin)
       end
     end
+
+    context "パラメータが不正な場合" do
+      it "リクエストが成功すること" do
+        sign_in admin
+        post exercise_categories_path, params: { exercise_category: FactoryBot.attributes_for(:exercise_category, :invalid) }
+        expect(response.status).to eq 302
+      end
+
+      it "カテゴリーが登録されないこと" do
+        expect do
+          sign_in admin
+          expect(
+            post exercise_categories_path, params: { exercise_category: FactoryBot.attributes_for(:exercise_category, :invalid) }
+          ).to_not change(exercise_category, :count)
+        end
+      end
+    end
+
+    describe "GET edit" do
+      it "リクエストが成功すること" do
+        expect do
+          sign_in admin
+          get edit_exercise_category_path exercise_category, xhr: true
+          expect(response.status).to eq 200
+        end
+      end
+
+      it "値が取得できていること" do
+        expect do
+          sign_in admin
+          get edit_exercise_category_path, params: { exercise_category: category_params }
+          expect(assigns :exercise_category).to eq category_params
+        end
+      end
+    end
+    
   end
   
 end
