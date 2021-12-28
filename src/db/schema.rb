54cba46916321d7aa972005f18028199c05d287d
masakiyusa <masa.aw1208@gmail.com>
2021-12-28 09:43:36 +0000

    myfood,recipeのvalidates修正

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -150,8 +150,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
     t.float "goal_body_weight"
     t.float "now_bodyfat_percentage"
     t.float "goal_bodyfat_percentage"
-    t.datetime "beginning_date", default: "2021-12-23 22:11:41"
-    t.datetime "target_date", default: "2021-12-24 22:11:41"
+    t.datetime "beginning_date", default: "2021-12-28 08:57:01"
+    t.datetime "target_date", default: "2021-12-29 08:57:01"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -165,7 +165,7 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
   end
 
   create_table "today_exercises", force: :cascade do |t|
-    t.date "start_time", default: "2021-12-24", null: false
+    t.date "start_time", default: "2021-12-28", null: false
     t.integer "exercise_time_hour", default: 0, null: false
     t.integer "exercise_time_min", default: 0, null: false
     t.float "body_weight", default: 0.0, null: false
