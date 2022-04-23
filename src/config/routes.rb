59423b97bd2668c9e0fc09ff4769fb0853d3c7f6
遊佐正樹 <yusamasaki@yusamasakinoMacBook-Pro-2.local>
2022-04-23 22:21:21 +0900

    routes構成修正(users/)

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -17,7 +17,8 @@ Rails.application.routes.draw do
     }
 
   resources :admins, only: [:show]
-  resources :users, only: [:show, :index] do
+  resources :users, only: [:show, :index]
+  namespace :users do
     get 'setting'
     get 'bodyweights/calender'
     get 'todaymeals/analysis'
