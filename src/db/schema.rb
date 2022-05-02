12fc40a79f608ec70a6b5197bfe8695a9bb85f86
遊佐正樹 <yusamasaki@yusamasakinoMacBook-Pro-2.local>
2022-05-02 10:14:12 +0900

    rails7_update

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -2,27 +2,54 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_014245) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_05_01_173253) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "provider"
     t.string "uid"
     t.string "username"
@@ -36,8 +63,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
     t.float "height"
     t.string "activity"
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_bmrs_on_user_id"
   end
 
@@ -46,8 +73,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
     t.float "body_weight"
     t.float "bodyfat_percentage"
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_bodyweights_on_user_id"
   end
 
@@ -55,8 +82,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
     t.date "start_time"
     t.float "calorie"
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_meals_analyses_on_user_id"
   end
 
@@ -70,8 +97,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
     t.float "dietary_fiber", default: 0.0
     t.float "salt", default: 0.0
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_myfoods_on_user_id"
   end
 
@@ -80,8 +107,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
     t.float "fat", default: 20.0
     t.float "carbo", default: 60.0
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_pfc_ratios_on_user_id"
   end
 
@@ -90,8 +117,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
     t.bigint "user_id"
     t.bigint "recipe_id"
     t.bigint "myfood_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["myfood_id"], name: "index_recipefoods_on_myfood_id"
     t.index ["recipe_id"], name: "index_recipefoods_on_recipe_id"
     t.index ["user_id"], name: "index_recipefoods_on_user_id"
@@ -108,8 +135,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
     t.float "salt"
     t.string "note"
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "id_digest"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
@@ -119,18 +146,18 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
     t.float "goal_body_weight"
     t.float "now_bodyfat_percentage"
     t.float "goal_bodyfat_percentage"
-    t.datetime "beginning_date", default: "2022-04-22 17:30:34"
-    t.datetime "target_date", default: "2022-04-23 17:30:34"
+    t.datetime "beginning_date", precision: nil, default: "2022-05-01 17:33:48"
+    t.datetime "target_date", precision: nil, default: "2022-05-02 17:33:48"
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["user_id"], name: "index_targetweights_on_user_id"
   end
 
   create_table "timezones", force: :cascade do |t|
     t.string "time_zone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "todaymeal_recipes", force: :cascade do |t|
@@ -140,8 +167,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
     t.bigint "user_id"
     t.bigint "timezone_id"
     t.bigint "recipe_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["recipe_id"], name: "index_todaymeal_recipes_on_recipe_id"
     t.index ["timezone_id"], name: "index_todaymeal_recipes_on_timezone_id"
     t.index ["user_id"], name: "index_todaymeal_recipes_on_user_id"
@@ -154,8 +181,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
     t.bigint "user_id"
     t.bigint "timezone_id"
     t.bigint "myfood_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["myfood_id"], name: "index_todaymeals_on_myfood_id"
     t.index ["timezone_id"], name: "index_todaymeals_on_timezone_id"
     t.index ["user_id"], name: "index_todaymeals_on_user_id"
@@ -166,15 +193,15 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "provider"
     t.string "uid"
     t.string "username"
@@ -183,6 +210,8 @@ ActiveRecord::Schema.define(version: 2021_12_23_014245) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bmrs", "users"
   add_foreign_key "bodyweights", "users"
   add_foreign_key "meals_analyses", "users"
